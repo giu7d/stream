d10419946e51f7c4ccf149ec3dc11d4572d7c5e4
Giuseppe Setem <giuseppe7d@gmail.com>
2023-07-27 17:09:28 -0300

    feat(core): create HSL client

---
+++ apps/core/lib/stream_core_web/controllers/live/live_controller.ex
@@ -0,0 +1,15 @@
+defmodule StreamCoreWeb.LiveController do
+  use StreamCoreWeb, :controller
+
+  alias Plug
+
+  def index(conn, %{"id" => id}) do
+    path = "output/#{id}"
+
+    if File.exists?(path) do
+      conn |> Plug.Conn.send_file(200, path)
+    else
+      conn |> Plug.Conn.send_resp(404, "File not found")
+    end
+  end
+end
--- apps/core/lib/stream_core_web/router.ex
+++ apps/core/lib/stream_core_web/router.ex
@@ -2,22 +2,23 @@ defmodule StreamCoreWeb.Router do
   use StreamCoreWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {StreamCoreWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {StreamCoreWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", StreamCoreWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
+    get("/live/:id", LiveController, :index)
   end
 
   # Other scopes may use custom stacks.
@@ -35,10 +36,10 @@ defmodule StreamCoreWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: StreamCoreWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: StreamCoreWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
