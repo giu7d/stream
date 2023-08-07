445945642985dbb1c73a12183f0056ec5af6be9c
Giuseppe Setem <giuseppe7d@gmail.com>
2023-08-07 10:10:04 -0300

    feat(core): add live stream custom view params

--- apps/core/lib/stream_core_web/live/live_stream/live_stream_live.ex
+++ apps/core/lib/stream_core_web/live/live_stream/live_stream_live.ex
@@ -4,7 +4,17 @@ defmodule StreamCoreWeb.LiveStreamLive do
   @stream_live_file Application.compile_env(:viewbox, :stream_live_file, "live.m3u8")
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, stream_live_file: @stream_live_file)}
+    {:ok,
+     assign(
+       socket,
+       stream_live_file: @stream_live_file,
+       stream_is_live?: true,
+       stream_views: 1000,
+       stream_user: %{
+         name: "Giuseppe Setem",
+         avatar_url: "https://avatars.githubusercontent.com/u/30274817?v=4"
+       }
+     )}
   end
 
   def unmount(_reason, _) do
