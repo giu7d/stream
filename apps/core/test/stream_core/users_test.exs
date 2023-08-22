cb1714de77e17e6bf84223f2fb65b53dff8df1e8
Giuseppe Setem <giuseppe7d@gmail.com>
2023-08-22 00:25:47 -0300

    test(core): add test to follower use cases

--- apps/core/test/stream_core/users_test.exs
+++ apps/core/test/stream_core/users_test.exs
@@ -66,4 +66,108 @@ defmodule StreamCore.UsersTest do
       assert {:error, :not_found} = Users.find_user_by(%{email: "wrong@mail.com"})
     end
   end
+
+  describe "add_follower/1" do
+    test "follows streamer successfully" do
+      user_follower = insert(:user)
+      user_streamer = insert(:user)
+
+      assert {:ok, follower} =
+               Users.add_follower(%{
+                 follower_id: user_follower.id,
+                 streamer_id: user_streamer.id
+               })
+
+      assert equals(follower.follower_id, user_follower.id)
+      assert equals(follower.streamer_id, user_streamer.id)
+    end
+
+    test "does not follows streamer if it does not exists" do
+      user_follower = insert(:user)
+
+      assert {:error, _} =
+               Users.add_follower(%{
+                 follower_id: user_follower.id,
+                 streamer_id: 99_999
+               })
+    end
+  end
+
+  describe "remove_follower/1" do
+    test "unfollows streamer successfully" do
+      follower = insert(:follower)
+
+      assert {:ok, _} =
+               Users.remove_follower(%{
+                 follower_id: follower.follower_id,
+                 streamer_id: follower.streamer_id
+               })
+    end
+
+    test "does not unfollows streamer if it does not exists" do
+      follower = insert(:follower)
+
+      assert {:error, :not_found} =
+               Users.remove_follower(%{
+                 follower_id: follower.follower_id,
+                 streamer_id: 99_999
+               })
+    end
+  end
+
+  describe "toggle_follower/1" do
+    test "add follows streamer successfully" do
+      user_follower = insert(:user)
+      user_streamer = insert(:user)
+
+      assert {:ok, _} =
+               Users.toggle_follower(%{
+                 follower_id: user_follower.id,
+                 streamer_id: user_streamer.id
+               })
+    end
+
+    test "remove follows streamer successfully" do
+      follower = insert(:follower)
+
+      assert {:ok, _} =
+               Users.toggle_follower(%{
+                 follower_id: follower.follower_id,
+                 streamer_id: follower.streamer_id
+               })
+    end
+
+    test "toggle follows streamer successfully" do
+      user_follower = insert(:user)
+      user_streamer = insert(:user)
+
+      assert {:ok, _} =
+               Users.toggle_follower(%{
+                 follower_id: user_follower.id,
+                 streamer_id: user_streamer.id
+               })
+
+      assert {:ok, _} =
+               Users.toggle_follower(%{
+                 follower_id: user_follower.id,
+                 streamer_id: user_streamer.id
+               })
+
+      assert {:error, :not_found} =
+               Users.remove_follower(%{
+                 follower_id: user_follower.id,
+                 streamer_id: user_streamer.id
+               })
+    end
+
+    test "does not unfollows streamer if it does not exists" do
+      user_follower = insert(:user)
+
+      assert {:error, _} =
+               Users.toggle_follower(%{
+                 follower_id: user_follower.id,
+                 streamer_id: 99_999
+               })
+    end
+  end
 end
--- apps/core/test/support/factories.ex
+++ apps/core/test/support/factories.ex
@@ -1,4 +1,5 @@
 defmodule StreamCore.Factories do
+  alias StreamCore.Users.Follower
   alias StreamCore.Users.User
   alias StreamCore.Mocks
 
@@ -14,6 +15,16 @@ defmodule StreamCore.Factories do
       username: username,
       password: password,
       hashed_password: Bcrypt.hash_pwd_salt(password)
+    }
+  end
+
+  def follower_factory do
+    user_follower = build(:user)
+    user_streamer = build(:user)
+
+    %Follower{
+      follower: user_follower,
+      streamer: user_streamer
     }
   end
 end
