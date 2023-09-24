226e8bfa068fbbb285d267b132309554822ab05f
Giuseppe Setem <giuseppe7d@gmail.com>
2023-09-24 01:54:38 -0300

    refactor(core): add view count support to live_stream_live

--- apps/core/lib/stream_core_web/live/live_stream/live_stream_live.ex
+++ apps/core/lib/stream_core_web/live/live_stream/live_stream_live.ex
@@ -1,8 +1,10 @@
 defmodule StreamCoreWeb.LiveStreamLive do
-  alias StreamCore.Users.User
-  alias StreamCore.Users
-  alias StreamCore.LiveStream.Stream
   alias StreamCore.LiveStream
+  alias StreamCore.LiveStream.Stream
+  alias StreamCore.LiveStreamMonitor
+  alias StreamCore.LiveStreamStatus
+  alias StreamCore.Users
+  alias StreamCore.Users.User
   alias StreamCoreWeb.Validator
 
   use StreamCoreWeb, :live_view
@@ -15,8 +17,16 @@ defmodule StreamCoreWeb.LiveStreamLive do
   def mount(params, _session, socket) do
     with {:ok, params} <- Validator.cast(params, @live_stream_params),
          {:ok, %User{} = user} <- Users.find_user(%{username: params.username}),
-         %Stream{} = live_stream <- LiveStream.find_live_stream(username: params.username) do
-      Phoenix.PubSub.subscribe(StreamCore.PubSub, "live:#{params.username}")
+         # refactor this
+         %Stream{} = live_stream <-
+           LiveStream.find_live_stream(username: params.username)
+           |> then(
+             &LiveStream.update_live_stream(&1.socket, fn stream ->
+               %{stream | viewer_count: stream.viewer_count + 1}
+             end)
+           ) do
+      LiveStreamStatus.subscribe(user)
+      LiveStreamMonitor.monitor(__MODULE__, params)
 
       {:ok,
        assign(
@@ -25,8 +35,8 @@ defmodule StreamCoreWeb.LiveStreamLive do
          stream: live_stream,
          user: user,
          stream_output_file: @stream_output_file,
-         stream_is_live?: true,
-         stream_views: 1000,
+         stream_is_live?: live_stream.is_live?,
+         stream_views: live_stream.viewer_count,
          stream_user: %{
            name: user.username,
            avatar_url: "https://avatars.githubusercontent.com/u/30274817?v=4"
@@ -35,10 +45,33 @@ defmodule StreamCoreWeb.LiveStreamLive do
     end
   end
 
-  def unmount(_reason, _params) do
-    # with {:ok, params} <- Validator.cast(params, @live_stream_params),
-    #      live_stream <- LiveStream.find_live_stream(username: params.username) do
-    #   {:ok, live_stream}
-    # end
+  def unmount(_reason, params) do
+    with {:ok, params} <- Validator.cast(params, @live_stream_params) do
+      # refactor this
+      LiveStream.find_live_stream(username: params.username)
+      |> then(
+        &LiveStream.update_live_stream(&1.socket, fn stream ->
+          %{stream | viewer_count: max(0, stream.viewer_count - 1)}
+        end)
+      )
+    end
+  end
+
+  def handle_info({:stream_changed, stream}, socket) do
+    {:noreply,
+     assign(socket,
+       stream: stream,
+       stream_views: stream.viewer_count,
+       stream_is_live?: stream.is_live?
+     )}
+  end
+
+  def handle_info({:stream_offline, stream}, socket) do
+    {:noreply,
+     assign(socket,
+       stream: stream,
+       stream_views: 0,
+       stream_is_live?: false
+     )}
   end
 end
