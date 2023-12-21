7566168300f92ea4e63d32bb18d45ce6b863be3a
Giuseppe Setem <giuseppe7d@gmail.com>
2023-12-21 17:26:18 -0300

    feat: improve system color stuff

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,61 +1,41 @@
-import { useEffect } from 'react'
-import { ImageBackground, View, useColorScheme } from 'react-native'
+import { View } from 'react-native'
 
 import classNames from 'classnames'
 import { StatusBar } from 'expo-status-bar'
-import { useColorScheme as useNativewindColorScheme } from 'nativewind'
 
-type ColorScheme = 'light' | 'dark'
+import useSystemColorScheme from '@/hooks/useSystemColorScheme'
 
 type TemplateProps = {
-  colorScheme?: ColorScheme
   children: React.ReactNode
 }
 
-function Template({ colorScheme = 'light', children }: TemplateProps) {
+function Template({ children }: TemplateProps) {
+  const colorScheme = useSystemColorScheme()
+
   return (
     <View
       className={classNames(
         'bg-white dark:bg-neutral-800',
-        'flex flex-grow p-8'
+        'flex flex-grow p-0 md:p-8'
       )}
     >
-      <ImageBackground source={{ uri: '../assets/plus.svg' }}>
-        <StatusBar style={colorScheme} />
-        <View className="flex flex-grow items-center justify-center">
-          {children}
-        </View>
-      </ImageBackground>
+      <StatusBar style={colorScheme} />
+      <View className="flex flex-grow justify-center items-center">
+        {children}
+      </View>
     </View>
   )
 }
 
 export default function Home() {
-  const colorScheme = useColorScheme() as ColorScheme
-  const nativewindColorScheme = useNativewindColorScheme()
-
-  useEffect(() => {
-    nativewindColorScheme.setColorScheme(colorScheme)
-  }, [colorScheme])
-
   return (
-    <Template colorScheme={colorScheme as any}>
+    <Template>
       <View
         className={classNames(
-          'w-4/5 h-4/5 rounded-lg',
+          'w-full h-[500] md:w-4/5 md:h-4/5 rounded-lg',
           'bg-neutral-200 dark:bg-neutral-950'
         )}
-      >
-        {/* <Text className={classNames("text-black dark:text-white")}>
-          Home page {dimension.width}x{dimension.height}
-        </Text>
-        <TouchableOpacity
-          className="bg-blue-400 rounded-lg px-6 py-2"
-          onPress={() => nativewindColorScheme.toggleColorScheme()}
-        >
-          <Text className="text-white">Button</Text>
-        </TouchableOpacity> */}
-      </View>
+      ></View>
     </Template>
   )
 }
--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,10 +1,8 @@
-const nativewind = import('nativewind/tailwind/css')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [nativewind()]
+  plugins: []
 }
